6038b68bd16ba5abf96dd4cbbf85c9239d3bdba4
NguyenQuocHao <nguyenquochao2000@gmail.com>
2026-04-10 09:26:57 -0400

    remove redundant code

--- src/services/AccessRepository.js
+++ src/services/AccessRepository.js
@@ -12,26 +12,3 @@ export class AccessRepository {
     }
   };
 }
-
-// export const getAccessList = () => {
-//   return roleAccessList;
-// };
-
-// export const getAccessForAdmin = () => {
-//   return roleAccessList.find((r) => r.role === "admin");
-// };
-
-// export const getAccessForDoctor = () => {
-//   return roleAccessList.find((r) => r.role === "doctor");
-// };
-
-// export const getAccessForPatient = () => {
-//   return roleAccessList.find((r) => r.role === "patient");
-// };
-
-// export const updateAccess = (access) => {
-//   const index = roleAccessList.findIndex((r) => r.role === access.role);
-//   if (index !== -1) {
-//     roleAccessList[index] = access;
-//   }
-// };
